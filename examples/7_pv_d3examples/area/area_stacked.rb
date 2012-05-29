b322fb9ef60aba00cf429aa567195a6d368bdd6e
Dafian <Andrea@Andrea-PC.(none)>
2012-05-30 00:08:54 +0200

    creation of a stacked area

---
+++ examples/7_pv_d3examples/area/area_stacked.rb
@@ -0,0 +1,104 @@
+# = Stacked Area
+# This example uses the Stack layout to stack areas one over another.
+$:.unshift(File.dirname(__FILE__)+"/../../../lib")
+require 'rubyvis'
+
+data = pv.range(4).map {|i|
+  pv.range(0, 10, 0.1).map {|x|
+    OpenStruct.new({:x=> x, :y=> Math.sin(x) + rand() * 0.5 + 2})
+  }
+}
+
+w = 400
+h = 200
+b = 20
+l = 20
+r = 10
+t = 5
+
+x = pv.Scale.linear(0, 9.9).range(0, w)
+y = pv.Scale.linear(0, 14).range(h, 0)
+
+#/* The root panel. */
+vis = pv.Panel.new() do
+
+  width w + r + l
+  height h +
+
+  group do
+    transform "translate(" + l.to_s + "," + t.to_s + ")"
+
+    rule do
+      data(x.ticks())
+      visible(lambda {|d| d!=0})
+      x1 x
+      x2 x
+      y1 h
+      y2 h + t
+      shape_rendering "crispEdges"
+      stroke_width "1"
+      stroke "rgb(0,0,0)"
+    end
+  end
+
+  group do
+    transform "translate(" + l.to_s + "," + t.to_s + ")"
+
+    label do
+      dy ".71em"
+      fill "rgb(0,0,0)"
+      text_anchor "middle"
+      x x
+      y h + t
+      text(x.tick_format)
+    end
+  end
+
+  group do
+    transform "translate(" + l.to_s + "," + t.to_s + ")"
+
+    rule do
+      data(y.ticks(3))
+      shape_rendering "crispEdges"
+      stroke(lambda {|d|  d!=0 ? "rgba(128,128,128,.2)" : "#000"})
+      stroke_width "1"
+      stroke_opacity{|d| d!=0 ? "0.2" : nil}
+      x1 0
+      x2 w
+      y1 y
+      y2 y
+    end
+  end
+
+  group do
+    transform "translate(" + l.to_s + "," + t.to_s + ")"
+
+    label do
+      text(y.tick_format)
+      dy ".35em"
+      fill "rgb(0,0,0)"
+      text_anchor "end"
+      x -3
+      y y
+    end
+  end
+
+  group do
+    transform "translate(" + l.to_s + "," + t.to_s + ")"
+
+    #/* The stack layout. */
+    vis.add(pv.Layout.Stack)
+    layers(data)
+    x(lambda {|d| x.scale(d.x)})
+    y(lambda {|d| y.scale(d.y)})
+    layer.add(pv.Area)
+  end
+
+end
+
+
+
+vis.render()
+f = File.new('C:\Users\Andrea\Desktop\stacked_charts.svg', "w")
+f.puts vis.to_svg
+f.close
