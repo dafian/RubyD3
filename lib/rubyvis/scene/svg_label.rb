bb8a89f41b82e4f7821f13d525ea5c89d0351b27
Dafian <dafian90@gmail.com>
2012-08-06 12:46:41 +0200

    integer labels are float

--- lib/rubyvis/scene/svg_label.rb
+++ lib/rubyvis/scene/svg_label.rb
@@ -40,7 +40,7 @@ module Rubyvis
           "text-anchor"=> s.text_anchor
         }, {
         "font"=> s.font, "text-shadow"=> s.text_shadow, "text-decoration"=> s.text_decoration})
-        e.text=s.text.frozen? ? s.text.dup : s.text
+        e.text=s.text.frozen? ? s.text.dup : (/^[0-9]+$/.match s.text.to_s ) ? s.text.to_s + ".0" : s.text.to_s #s.text
         gvs.add_element(e)
       end
       gvs
